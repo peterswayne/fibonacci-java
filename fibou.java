1308fb5916bd03ea8d4ab4a8b0034f7dc4ecdaf0
peterswayne <c16778095@mydit.ie>
2018-09-25 22:01:29 +0100

    Update fibou.java

--- fibou.java
+++ fibou.java
@@ -1,7 +1,7 @@
 package Test;
 
 public class quat {
-
+	//initialise variables
 	int x=0;
 	int first=1;
 	int second=1;
@@ -11,21 +11,28 @@ public class quat {
 	//constructor 
 	public quat(int Number)
 	{
-	
+		//rints the number
 		System.out.println("The number is "+Number);
+		//loops while x is less than the number passes
 		while( x <Number)
 		{
+			//checks if x is less than or equal to 1
 			if (x<=1)
 			{
+				//prints 1
 				System.out.println(1);
-				
+				//x is set to 2
 				x=2;
 			}
-			
+			//product is set to the sum of the first and second number
 			product=first+second;
+			//prints the second number
 			System.out.println(product);
+			//first is set as second
 			first=second;
+			//second set as product
 			second=product;
+			//x is incremented
 			x++;
 		}
 	}
