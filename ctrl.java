08d22e17089c8d496ac83ba5ee66aa93ba4a4c0a
peterswayne <c16778095@mydit.ie>
2018-09-25 21:56:12 +0100

    Update ctrl.java

--- ctrl.java
+++ ctrl.java
@@ -5,10 +5,15 @@ public class ctrl {
 
 	public static void main(String args[]) 
 	{
+		
 		System.out.println("Enter in the number of emmmm\n");
+		//getting user input
 		Scanner sc = new Scanner(System.in);
+		//placing input into variable
 		int var=sc.nextInt();
+		//creating new object
 		quat a1 = new quat(var); 
+		//display 
 		a1.show(3);
 	}
 
